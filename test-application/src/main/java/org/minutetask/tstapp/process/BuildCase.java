8e5eec2925f3fe89c5bf0ab923ab3f4421dbafa4
janko <janko@JK78.home>
2025-03-07 05:58:31 +0100

    - prepare new example

---
+++ test-application/src/main/java/org/minutetask/tstapp/process/BuildCase.java
@@ -0,0 +1,45 @@
+package org.minutetask.tstapp.process;
+
+/*-
+ * ========================LICENSE_START=================================
+ * casecore-test-application
+ * %%
+ * Copyright (C) 2025 Jan Komrska
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
+
+import org.minutetask.casecore.annotation.ClosedRef;
+import org.minutetask.casecore.annotation.IdRef;
+import org.minutetask.casecore.annotation.ServiceRef;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class BuildCase {
+    @IdRef
+    private Long caseId;
+
+    @ClosedRef
+    private boolean closed;
+
+    private Long projectId;
+
+    @ServiceRef(BuildFlow.class)
+    private Class<? extends BuildFlow> flow;
+}
